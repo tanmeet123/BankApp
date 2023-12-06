1b643d3765c6b0ac3f809ffab84008fc53df788c
Tanmeet Kaur <tankaur4@publicisgroupe.net>
2023-12-06 11:45:28 +0530

    base

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+
+    BufferedReader buff;
+    InputStreamReader isr;
+    public Main() {
+        if(isr == null)
+            isr = new InputStreamReader(System.in);
+        if(buff==null)
+            buff = new BufferedReader(isr);
+    }
+
+    int selectedBank, selectedOperation;
     public static void main(String[] args) {
-        System.out.println("Hey");
-        //comment
+        Main obj = new Main();
+        System.out.println("Welcome to IBS\nPlease select your bank\n1. ICICI\n2. HDFC\n3. SBI\n4. AXIS\n5. IDFC");
+
+        try {
+            obj.selectedBank = Integer.parseInt(obj.buff.readLine());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Customer Selected " + obj.selectedBank);
+        System.out.println("Select your choice\n1. Deposit\n2. Withdrawl\n3. OpenFD\n4. Apply Loan\n5. Apply CC");
+
+        try {
+            obj.selectedOperation = Integer.parseInt(obj.buff.readLine());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Customer Selected " + obj.selectedOperation);
+
     }
 }
---
+++ src/RBI.java
@@ -0,0 +1,12 @@
+public class RBI {
+    float balance;
+
+    public void depositMoney() {}
+    public void withdrawMoney(){}
+    public void openFD(float amount, float ROI, int years) {}
+    public void applyLoan(String loanType, float amount, float ROI, int years) {}
+    public void applyCreditCard() {}
+    public float getBalance() {
+        return 0.0f;
+    }
+}
